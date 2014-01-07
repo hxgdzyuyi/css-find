3912e5705190e3cf97e611691f495129a9173900
hxgdzyuyi <hxgdzyuyi@gmail.com>
2014-01-07 14:55:15 +0800

    添加 css-find 的基础功能， 可以搜索 css 属性

---
+++ bin/css-find.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+
+var finder = require('../lib/css-find')
+  , args = process.argv.slice(2)
+  , fs = require('fs')
+  , clc = require('cli-color')
+  , prop
+  , files = []
+
+while (args.length) {
+  arg = args.shift()
+  switch(arg) {
+    case '-f':
+    case '--css-file':
+      files.push(args.shift())
+      break;
+    default:
+      prop = arg
+  }
+}
+
+var str = ''
+
+files.forEach(function(filePath) {
+  str += fs.readFileSync(filePath, 'utf-8')
+})
+
+var results = finder.search(str, prop)
+
+if (!results) { return }
+
+results.forEach(function(result) {
+  console.log(clc.green(prop + ':' + result.value))
+  result.selectors.forEach(function(selector, index) {
+    console.log(selector)
+    console.log(' ' +(index + 1) + ') ' + selector)
+  })
+})
