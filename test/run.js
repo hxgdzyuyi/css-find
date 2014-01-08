e3f76126c2dcb7d1b09af9fea9d2163a98d9b089
hxgdzyuyi <hxgdzyuyi@gmail.com>
2014-01-08 15:24:09 +0800

    添加查找属性的测试

--- test/run.js
+++ test/run.js
@@ -1,11 +1,34 @@
-var should = require('should');
+var should = require('should')
+  , finder = require('../lib/css-find')
+  , fs = require('fs')
 
-var user = {
-    name: 'css-find'
-};
+describe('should find right prop', function(){
+  var string = fs.readFileSync(__dirname
+    + '/../fixture/find-prop.css', 'utf-8')
 
-describe('integration', function(){
-  it('run', function(){
-    user.should.have.property('name', 'css-find');
+  it('find `z-index`', function() {
+    var expected =
+          [ { value: '12'
+            , selectors: [ '.z-index12' ] }
+
+          , { value: '13'
+            , selectors: [ '.red .z-index12,\n.wrapper', '.z-index13' ] }
+          ]
+
+    finder.search(string, 'z-index')
+      .should.eql(expected)
+  })
+
+  it('find `font-size`', function() {
+    var expected =
+          [ { value: '12px'
+            , selectors: [ '.z-index13' ] }
+
+          , { value: '13px'
+            , selectors: [ '.red .z-index12,\n.wrapper' ] }
+          ]
+
+    finder.search(string, 'font-size')
+      .should.eql(expected)
   })
 })
